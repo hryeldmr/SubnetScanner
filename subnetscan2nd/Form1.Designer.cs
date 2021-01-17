6e9ccb2f59ce85fce2128bec43575a8fee030623
Hry Eldmr <hry_97@hotmail.com>
2021-01-17 17:18:45 +0300

    Add project files.

---
+++ subnetscan2nd/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace subnetscan2nd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmdScan = new System.Windows.Forms.Button();
+            this.listVAddr = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.cmdStop = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.conMenuStripIP = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shutdownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rebootToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.powerOffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.conMenuStripIP.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmdScan
+            // 
+            this.cmdScan.Location = new System.Drawing.Point(265, 10);
+            this.cmdScan.Margin = new System.Windows.Forms.Padding(4);
+            this.cmdScan.Name = "cmdScan";
+            this.cmdScan.Size = new System.Drawing.Size(69, 26);
+            this.cmdScan.TabIndex = 0;
+            this.cmdScan.Text = "Tarama";
+            this.cmdScan.UseVisualStyleBackColor = true;
+            this.cmdScan.Click += new System.EventHandler(this.cmdScan_Click);
+            // 
+            // listVAddr
+            // 
+            this.listVAddr.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listVAddr.Location = new System.Drawing.Point(-2, 0);
+            this.listVAddr.Margin = new System.Windows.Forms.Padding(4);
+            this.listVAddr.Name = "listVAddr";
+            this.listVAddr.Size = new System.Drawing.Size(451, 221);
+            this.listVAddr.TabIndex = 1;
+            this.listVAddr.UseCompatibleStateImageBehavior = false;
+            this.listVAddr.View = System.Windows.Forms.View.Details;
+            this.listVAddr.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listVAddr_MouseClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IP";
+            this.columnHeader1.Width = 95;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Host Adı";
+            this.columnHeader2.Width = 180;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Durum";
+            this.columnHeader3.Width = 59;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtIP);
+            this.panel1.Controls.Add(this.cmdStop);
+            this.panel1.Controls.Add(this.cmdScan);
+            this.panel1.Location = new System.Drawing.Point(5, 250);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(432, 86);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.lblStatus);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(18, 49);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(387, 28);
+            this.panel2.TabIndex = 5;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(57, 5);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 17);
+            this.lblStatus.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-1, 5);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Durum :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "e.g. 10.172.44";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Alt Ağ :";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(111, 11);
+            this.txtIP.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(145, 22);
+            this.txtIP.TabIndex = 2;
+            // 
+            // cmdStop
+            // 
+            this.cmdStop.Enabled = false;
+            this.cmdStop.Location = new System.Drawing.Point(343, 10);
+            this.cmdStop.Margin = new System.Windows.Forms.Padding(4);
+            this.cmdStop.Name = "cmdStop";
+            this.cmdStop.Size = new System.Drawing.Size(69, 26);
+            this.cmdStop.TabIndex = 1;
+            this.cmdStop.Text = "Dur";
+            this.cmdStop.UseVisualStyleBackColor = true;
+            this.cmdStop.Click += new System.EventHandler(this.cmdStop_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkGray;
+            this.label4.Location = new System.Drawing.Point(-6, 222);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(432, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ayrıntılı bilgi için ana bilgisayarın IP sine sağ tıklayın";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(5, 341);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(432, 16);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // conMenuStripIP
+            // 
+            this.conMenuStripIP.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.conMenuStripIP.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showInfoToolStripMenuItem,
+            this.shutdownToolStripMenuItem,
+            this.rebootToolStripMenuItem,
+            this.powerOffToolStripMenuItem});
+            this.conMenuStripIP.Name = "conMenuStripIP";
+            this.conMenuStripIP.Size = new System.Drawing.Size(177, 100);
+            // 
+            // showInfoToolStripMenuItem
+            // 
+            this.showInfoToolStripMenuItem.Name = "showInfoToolStripMenuItem";
+            this.showInfoToolStripMenuItem.Size = new System.Drawing.Size(176, 24);
+            this.showInfoToolStripMenuItem.Text = "Bilgileri Göster";
+            this.showInfoToolStripMenuItem.Click += new System.EventHandler(this.showInfoToolStripMenuItem_Click);
+            // 
+            // shutdownToolStripMenuItem
+            // 
+            this.shutdownToolStripMenuItem.Name = "shutdownToolStripMenuItem";
+            this.shutdownToolStripMenuItem.Size = new System.Drawing.Size(176, 24);
+            this.shutdownToolStripMenuItem.Text = "Kapat";
+            this.shutdownToolStripMenuItem.Click += new System.EventHandler(this.shutdownToolStripMenuItem_Click);
+            // 
+            // rebootToolStripMenuItem
+            // 
+            this.rebootToolStripMenuItem.Name = "rebootToolStripMenuItem";
+            this.rebootToolStripMenuItem.Size = new System.Drawing.Size(176, 24);
+            this.rebootToolStripMenuItem.Text = "Yeniden Başlat";
+            this.rebootToolStripMenuItem.Click += new System.EventHandler(this.rebootToolStripMenuItem_Click);
+            // 
+            // powerOffToolStripMenuItem
+            // 
+            this.powerOffToolStripMenuItem.Name = "powerOffToolStripMenuItem";
+            this.powerOffToolStripMenuItem.Size = new System.Drawing.Size(176, 24);
+            this.powerOffToolStripMenuItem.Text = "Gücü Kapat";
+            this.powerOffToolStripMenuItem.Click += new System.EventHandler(this.powerOffToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 362);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listVAddr);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Alt Ağ Tarayıcısı";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.conMenuStripIP.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdScan;
+        private System.Windows.Forms.ListView listVAddr;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button cmdStop;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ContextMenuStrip conMenuStripIP;
+        private System.Windows.Forms.ToolStripMenuItem showInfoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shutdownToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rebootToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem powerOffToolStripMenuItem;
+    }
+}
+
---
+++ subnetscan2nd/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation; // mac adresleri lazm olcak ağda islem yapacağız
+using System.Threading; //eş zamanlı işlem yapmak için
+using System.Net; // ping vs internet gerektiren kodlarımız için
+using System.Management;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO;
+
+
+namespace subnetscan2nd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            lblStatus.ForeColor = System.Drawing.Color.Red; //boş olanlar kırmızı renk alacak sistemsel olarak
+            lblStatus.Text = "Boş";
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        Thread myThread = null;
+
+        public void scan(string subnet) 
+        {
+            Ping benimping;
+            PingReply cevap;
+            IPAddress adres;
+            IPHostEntry host; // burada gerekli değişkenlerimizi aldık
+
+            progressBar1.Maximum = 254;
+            progressBar1.Value = 0;//ipler 0dan başlar 255 kadardır bu nedenle progressbar değerlerini 
+                                   //0 254 arasında değiştirdik
+            listVAddr.Items.Clear();//listbox temizliyoruz her taramada
+
+            for (int i = 1; i < 255; i++)
+            //programımızda alt alan adı yazdırıyoruz bu nedir örneğin 192.168.1
+            //ancak klasik bi ip 192.168.1 ile 192.168.1.255 arasındadır
+            //bu sebeple döngü ile her ağa ping atarak bu alandan geri dönüş olup
+            //olmadığına bakacağız for 1 ile 255 arası çalıştırdık
+            {
+                string subnetn = "." + i.ToString();//örnek 192.168.1 değerine . ekledik daha sonra i değerini verdik
+                                                    //ilk dönmesinde değer 192.168.1.1 oldu
+                benimping = new Ping();
+                cevap = benimping.Send(subnet+subnetn, 900);//daha sonra oluşturdumuz ipye ping attik
+
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+                lblStatus.Text = "Taranıyor : " + subnet + subnetn;//taranıyor yazısı ve ip gösterdik lbl rengi bu sürede yesil
+
+                if (cevap.Status == IPStatus.Success)
+                { //eğer olumlu dönerse listboxa eklicek hostu ve ipimizi kaydedecek
+                    try 
+                     {
+                         adres = IPAddress.Parse(subnet + subnetn);
+                         host = Dns.GetHostEntry(adres);
+
+                         listVAddr.Items.Add(new ListViewItem(new String[] { subnet + subnetn, host.HostName, "Hazır" }));
+                     }
+                     catch { MessageBox.Show( subnet+subnetn, "için ana makine adı alınamadı", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+                 }
+                 progressBar1.Value += 1;//dönmezse bişe yapmasına gerek yok işlemlere devam
+            }                    
+            cmdScan.Enabled = true;
+            cmdStop.Enabled = false;
+            txtIP.Enabled = true;
+            lblStatus.Text = "Bitti!";
+            int count = listVAddr.Items.Count;
+            MessageBox.Show("Tarama Tamamlandı!" + count.ToString() + " Host Bulundu.", "Tamamlandı", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+        }
+
+        public void query(string host) 
+        {
+            //string acc;
+            //string os;
+            //string board;
+            //string biosVersion;
+            string temp = null;
+            string[] _searchClass = {"Win32_ComputerSystem", "Win32_OperatingSystem", "Win32_BaseBoard", "Win32_BIOS" };
+            string[] param = { "Kullanıcı Adı", "Başlık", "Ürün", "Açıklama"};
+
+            lblStatus.ForeColor = System.Drawing.Color.Green;
+
+            for (int i = 0; i <= _searchClass.Length-1; i++)
+            {
+                lblStatus.Text = "Bilgi Alınıyor.";
+                try
+                {
+                    ManagementObjectSearcher searcher = new ManagementObjectSearcher("\\\\" + host + "\\root\\CIMV2", "SELECT *FROM "+_searchClass[i]);
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        lblStatus.Text = "Bilgi Alınıyor .";
+                    
+                        temp += obj.GetPropertyValue(param[i]).ToString() + "\n";
+                        if (i == _searchClass.Length - 1)
+                        {
+                            lblStatus.Text = "Tamamlandı!";
+                            MessageBox.Show(temp, "Host Bilgisi : " + host, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            break;
+                        }
+                        lblStatus.Text = "Bilgi Alınıyor. . .";
+                    }
+                }
+                catch (Exception ex) { MessageBox.Show("WMI sorgusunda hata.\n\n" + ex.ToString(), "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error); break; } 
+            }
+        }
+
+        public void controlSys(string host, int flags)
+        {
+            #region 
+            /*
+             *Flags:
+             *  0 (0x0)Log Off
+             *  4 (0x4)Forced Log Off (0 + 4)
+             *  1 (0x1)Shutdown
+             *  5 (0x5)Forced Shutdown (1 + 4)
+             *  2 (0x2)Reboot
+             *  6 (0x6)Forced Reboot (2 + 4)
+             *  8 (0x8)Power Off
+             *  12 (0xC)Forced Power Off (8 + 4)
+             */
+            #endregion
+
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("\\\\" + host + "\\root\\CIMV2", "SELECT *FROM Win32_OperatingSystem");
+
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    ManagementBaseObject inParams = obj.GetMethodParameters("Win32Shutdown");
+
+                    inParams["Flags"] = flags;
+
+                    ManagementBaseObject outParams = obj.InvokeMethod("Win32Shutdown", inParams, null);
+                }
+            }
+            catch (ManagementException manex) { MessageBox.Show("Hata:\n\n" + manex.ToString(), "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+            catch (UnauthorizedAccessException unaccex) { MessageBox.Show("Yetkili?\n\n"+unaccex.ToString(), "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        private void cmdScan_Click(object sender, EventArgs e)
+        {
+            if (txtIP.Text == string.Empty)
+            {
+                MessageBox.Show("IP Adresi Girilmedi.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                myThread = new Thread(() => scan(txtIP.Text));
+                myThread.Start();
+
+                if (myThread.IsAlive == true)
+                {
+                    cmdStop.Enabled = true;
+                    cmdScan.Enabled = false;
+                    txtIP.Enabled = false;
+                }
+            }      
+        }
+
+        private void cmdStop_Click(object sender, EventArgs e)
+        {
+            myThread.Suspend();
+            cmdScan.Enabled = true;
+            cmdStop.Enabled = false;
+            txtIP.Enabled = true;
+            lblStatus.ForeColor = System.Drawing.Color.Red;
+            lblStatus.Text = "Boş";
+        }
+
+        private void listVAddr_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if(listVAddr.FocusedItem.Bounds.Contains(e.Location)==true)
+                {
+                    conMenuStripIP.Show(Cursor.Position);
+                }
+            }
+            //else if(e.Button == MouseButtons.Left)
+            //{
+            //    if (listVAddr.FocusedItem.Bounds.Contains(e.Location) == true)
+            //    {
+            //        if (listVAddr.SelectedItems.Count > 0)
+            //        {
+            //            string host = listVAddr.SelectedItems[0].Text.ToString();
+            //            Thread qThread = new Thread(() => query(host));
+            //            qThread.Start();
+            //        }
+            //    }
+            //}
+        }
+
+        private void showInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string host = listVAddr.SelectedItems[0].Text.ToString();
+            Thread qThread = new Thread(() => query(host));
+            qThread.Start();
+        }
+
+        private void shutdownToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string host = listVAddr.SelectedItems[0].Text.ToString();
+            controlSys(host, 5);
+        }
+
+        private void rebootToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string host = listVAddr.SelectedItems[0].Text.ToString();
+            controlSys(host, 6);
+        }
+
+        private void powerOffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string host = listVAddr.SelectedItems[0].Text.ToString();
+            controlSys(host, 12);
+        }
+    }
+}
